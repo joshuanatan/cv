0605b98d345adac7baddcd825a52cac629a1b8b3
joshuanatan <joshuanatan.jn@gmail.com>
2019-09-18 22:49:57 +0700

    Update slideshow.php

--- application/views/welcome/slideshow.php
+++ application/views/welcome/slideshow.php
@@ -1,4 +1,4 @@
-<div class="slider_area slideshowGradient" id = "welcome" style = "background-size:150%;background-image:url(<?php echo base_url();?>assets/img/banner/wallpaper.jpg)">
+<div class="slider_area slideshowGradient" id = "welcome" style = "background-size:150% 100%;background-image:url(<?php echo base_url();?>assets/img/banner/wallpaper.jpg)">
     <div class="slider_active owl-carousel">
         <div class="single_slider overlay2 d-flex align-items-center justify-content-center " >
             <div class="container">
